6c3251ebf2b8eef413bab2d49deafd9887989252
hugoymh <34024260+hugoymh@users.noreply.github.com>
2017-12-11 15:10:23 +0800

    Initial commit

---
+++ JustClimbTrial/Views/Pages/JustClimbHome.xaml.cs
@@ -0,0 +1,33 @@
+﻿using JustClimbTrial.Kinect;
+using Microsoft.Kinect;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace JustClimbTrial.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for JustClimbHome.xaml
+    /// </summary>
+    public partial class JustClimbHome : Page
+    {
+       
+
+        public JustClimbHome()
+        {
+
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            ModeSelect modeSelectPage = new ModeSelect();
+            this.NavigationService.Navigate(modeSelectPage);
+
+            (this.Parent as MainWindow).KinectManagerClient.ColorImageSourceArrived -= (this.Parent as MainWindow).HandleColorImageSourceArrived;
+        }
+
+
+    }
+}
